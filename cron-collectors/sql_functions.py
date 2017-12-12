afae0b16c4a980637ec7df636790611e68a3cd5a
Maurice Zeitouni <morriszeitouni@Maurices-MacBook-Pro.local>
2017-12-12 16:18:13 -0500

    changed to gcp code

--- cron-collectors/sql_functions.py
+++ cron-collectors/sql_functions.py
@@ -1,3 +1,15 @@
-from sql import *
-import mysql.connector
-from mysql.connector import Error
+import os
+
+import MySQLdb
+import webapp2
+
+def connect():
+    db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
+
+ 
+ 
+if __name__ == '__main__':
+    connect()
