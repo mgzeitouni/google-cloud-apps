68f8b9e94a8463cad4fd06ba26db9df92b5bfbf6
slave 2 raspi <mgzeitouni@gmail.com>
2017-12-12 21:31:30 +0000

    back to connectors

--- cron-collectors/sql_functions.py
+++ cron-collectors/sql_functions.py
@@ -1,15 +1,22 @@
-import os
-
-import MySQLdb
-import webapp2
-
-def connect():
-    db = MySQLdb.connect(
-            unix_socket=cloudsql_unix_socket,
-            user=CLOUDSQL_USER,
-            passwd=CLOUDSQL_PASSWORD)
-
+import mysql.connector
+from mysql.connector import Error
  
+def connect():
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect(host='35.196.174.154',
+                                       database='KarteesMasterData',
+                                       user='root',
+                                       password='kartees12')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+ 
+    except Error as e:
+        print(e)
+ 
+   # finally:
+    #    conn.close()
+ #
  
 if __name__ == '__main__':
     connect()
